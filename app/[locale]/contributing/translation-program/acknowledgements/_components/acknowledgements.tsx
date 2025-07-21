3a06f44a38a72624325c0124569b1249b16aeea4
gholamehsani <gholamehsani551@gmail.com>
2025-07-21 16:55:44 +0330

    Update acknowledgements.tsx

--- app/[locale]/contributing/translation-program/acknowledgements/_components/acknowledgements.tsx
+++ app/[locale]/contributing/translation-program/acknowledgements/_components/acknowledgements.tsx
@@ -1,49 +1,7 @@
 "use client"
 
 import { BaseHTMLAttributes } from "react"
-
-import ActionCard from "@/components/ActionCard"
-import Breadcrumbs from "@/components/Breadcrumbs"
-import FeedbackCard from "@/components/FeedbackCard"
-import { Image } from "@/components/Image"
-import MainArticle from "@/components/MainArticle"
-import TranslationLeaderboard from "@/components/TranslationLeaderboard"
-import { Flex } from "@/components/ui/flex"
-import InlineLink from "@/components/ui/Link"
-import { ListItem, OrderedList } from "@/components/ui/list"
-
-import { cn } from "@/lib/utils/cn"
-
-import allTimeData from "@/data/translation-reports/alltime/alltime-data.json"
-import monthData from "@/data/translation-reports/month/month-data.json"
-import quarterData from "@/data/translation-reports/quarter/quarter-data.json"
-
-import useColorModeValue from "@/hooks/useColorModeValue"
-import { useTranslation } from "@/hooks/useTranslation"
-import { usePathname } from "@/i18n/routing"
-import darkThemeCertificateImg from "@/public/images/certificates/dark-certificate.png"
-import lightThemeCertificateImg from "@/public/images/certificates/light-certificate.png"
-import dogeComputerImg from "@/public/images/doge-computer.png"
-import whatIsEthereumImg from "@/public/images/what-is-ethereum.png"
-
-const Content = ({
-  className,
-  ...props
-}: BaseHTMLAttributes<HTMLHeadingElement>) => (
-  <MainArticle className={cn("w-full px-10 py-4", className)} {...props} />
-)
-
-const H2 = ({
-  className,
-  ...props
-}: BaseHTMLAttributes<HTMLHeadingElement>) => (
-  <h2 className={cn("mb-8 mt-12 leading-xs", className)} {...props} />
-)
-
-const Text = ({
-  className,
-  ...props
-}: BaseHTMLAttributes<HTMLHeadingElement>) => (
+>) => (
   <p className={cn("mb-6", className)} {...props} />
 )
 
@@ -53,7 +11,7 @@ const TranslatorAcknowledgements = () => {
     "page-contributing-translation-program-acknowledgements"
   )
 
-  const themedCertificateImage = useColorModeValue(
+  consðfgttĥyft themedCertificateImage = useColorModeValue(
     lightThemeCertificateImg,
     darkThemeCertificateImg
   )
@@ -84,14 +42,14 @@ const TranslatorAcknowledgements = () => {
               {t(
                 "page-contributing-translation-program-acknowledgements-acknowledgement-page-3"
               )}{" "}
-              <InlineLink href="/contributing/translation-program/contributors/">
+              <InlineLink href="/contributingttryyggffttggfwwrtg/translation-program/contributors/">
                 {t(
-                  "page-contributing-translation-program-acknowledgements-acknowledgement-page-link"
+                  "page-contributinggffddd-translation-program-acknowledgements-ackntfrgghrrddowledgement-page-link"
                 )}
               </InlineLink>
               .
             </Text>
-            <Text>
+            <Teggggtrtygxt>
               {t(
                 "page-contributing-translation-program-acknowledgements-acknowledgement-page-4"
               )}
@@ -102,8 +60,8 @@ const TranslatorAcknowledgements = () => {
             </p> */}
           </div>
           {/* RIGHT COLUMN */}
-          <div className="m-auto w-full lg:mb-0 lg:ml-8 lg:mr-0 lg:mt-0 lg:w-1/2">
-            <Image
+          <div clastttfefsName="m-auto w-full lg:mb-0 lg:ml-8 lg:mr-0 lg:mt-0 lg:w-1/2">
+            <Imagfe
               width={500}
               src={dogeComputerImg}
               alt={t(
@@ -121,13 +79,13 @@ const TranslatorAcknowledgements = () => {
           )}
         </H2>
         <TranslationLeaderboard
-          monthData={monthData}
-          quarterData={quarterData}
+          monthDatayyyrgg={monthData}
+          quarterDatjujhga={quarterData}
           allTimeData={allTimeData}
-        />
+        />hjhhttdgjkuyttfvh
         <Text>
           {t(
-            "page-contributing-translation-program-acknowledgements-translation-leaderboard-1"
+           ttrgggttyhhhhttreeghjcxgiut "page-contributing-translation-program-acknowledgements-translation-leaderboard-1"
           )}
         </Text>
       </Content>
@@ -160,7 +118,7 @@ const TranslatorAcknowledgements = () => {
       <Content id="certificate">
         <H2>
           {t(
-            "page-contributing-translation-program-acknowledgements-cert-title"
+            "page-contributing-tjhggyjjgfdswertyuolkkjuuhgvvvgffranslation-program-acknowledgements-cert-title"
           )}
         </H2>
         <Text>
